36e220b5b7a836d9e252daae0d6f602c323af5fd
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2025-02-05 00:17:28 +0900

    add: PostgreSQL データベース接続とユーザーエンティティのマイグレーションを追加

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
-	"fmt"
+  "fmt"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"golang-user-auth/internal/entity"
+	"golang-user-auth/internal/repository"
+	"golang-user-auth/internal/usecase"
+	httpDeliver "golang-user-auth/internal/delivery/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	if err := db.AutoMigrate(&entity.User{}); err != nil {
+		log.Fatalf("failed to migrate database: %v", err)
+	}
+
+	r := gin.Default()
+
+	userRepo := repository.NewUserRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepo)
+	httpDeliver.NewUserHandler(r, userUsecase)
+
+	log.Println("server is running on port 8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("failed to run server: %v", err)
+	}
 }
